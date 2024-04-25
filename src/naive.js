d28f8a091669b0a9622364ab99a0d8725a124449
yingbin.yang@midea.com <yingbin.yang@midea.com>
2024-04-25 09:39:34 +0800

    舒服了

--- src/naive.js
+++ src/naive.js
@@ -9,9 +9,10 @@ const REJECTED = 'rejected'
 function A(f) {
   this.state = PENDING
   this.result = null
+  // 如果then执行的时候是PENDING的话，push一个进去用于未来回调
   this.callbacks = []
 
-  // 状态迁移
+  // 状态迁移，回调执行
   const re = (result)=>{
     transition(this, FULFILLED, result)
   }
@@ -19,25 +20,12 @@ function A(f) {
     transition(this, REJECTED, reason)
   }
 
-  // 只回调一次
-  let promiseCalled = false;
-  // 当前promise异步执行成功后回调
-  const resolvePromise = (y) => {
-    if (promiseCalled) return;
-    promiseCalled = true;
-    // y很多种情况:Promise、thenable、非thenable
-    // 递归下去直到递归停止，将递归停止时的值作为当前promise异步执行的结果
-    pRP(this, y, re, rj);
-  };
-  // 当前promise异步执行失败后回调
-  const rejectPromise = (r) => {
-    if (promiseCalled) return;
-    promiseCalled = true;
-    rj(r);
-  };
-
-  // f实际上可以看作是第一个自定义的then function
-  f && f(resolvePromise, rejectPromise, this)
+  // 马上执行外部传入的异步处理函数
+  try{
+    f(re, rj, this)
+  }catch(e){
+    rj(e)
+  }
 }
 const transition = (promise, state, result) => {
   if (promise.state !== PENDING) return
@@ -46,35 +34,36 @@ const transition = (promise, state, result) => {
   setTimeout(()=>{
     while(promise.callbacks.length) {
       const callback = promise.callbacks.shift()
-      handleCallback(callback,state,result,callback.p)
+      // then的回调函数处理
+      handleCallback(callback,state,result)
     }
   }, 0)
 }
 
 A.prototype.then = function(onFulfilled, onRejected) {
-  return new A((resolve, reject, p) => {
-    // 先假设resolve可以让该promise迁移至fulfilled，参数是result，resolve里可以拿到该promise的引用
-    // 先假设reject可以让该promise迁移至rejected，参数是reason
-    const callback = { onFulfilled, onRejected, resolve, reject, p }
+  // 最终返回一个promise2
+  return new A((resolve, reject, me) => {
+    const callback = { onFulfilled, onRejected, resolve, reject, me}
 
     if (this.state === PENDING) {
       this.callbacks.push(callback)
     } else {
-      setTimeout(() => handleCallback(callback, this.state, this.result, p), 0)
+      setTimeout(() => handleCallback(callback, this.state, this.result), 0)
     }
   })
 }
 
-// p是跟callback里resolve对应的promise
-function handleCallback(callback, state, result, p) {
-  const { onFulfilled, onRejected, resolve, reject } = callback
+// 根据onFulfilled和onRejected执行的结果执行resolve或者reject，影响resolve对应的promise2的状态
+function handleCallback(callback, state, result) {
+  // 某一次then的callback
+  const { onFulfilled, onRejected, resolve, reject, me } = callback
   try {
     if (state === FULFILLED) {
-      // TODO resolve(onFulfilled(result))
-      isFunction(onFulfilled) ? pRP(p, onFulfilled(result), resolve, reject) : resolve(result)
+      // onFulfilled是函数的话执行，它的返回值用于执行pRP；否则沿用上一个promise的状态
+      isFunction(onFulfilled) ? pRP(me, onFulfilled(result), resolve, reject) : resolve(result)
     } else if (state === REJECTED) {
-      // TODO resolve(onRejected(result))
-      isFunction(onRejected) ? pRP(p, onRejected(result), resolve, reject) : reject(result)
+      // onRejected是函数的话执行，它的返回值用于执行pRP；否则沿用上一个promise的状态
+      isFunction(onRejected) ? pRP(me, onRejected(result), resolve, reject) : reject(result)
     }
   } catch (e) {
     reject(e)
@@ -82,34 +71,26 @@ function handleCallback(callback, state, result, p) {
 }
 
 // Promise Resolution Procedure
-function pRP(promise, resultOrReason, resolve, reject) {
-  if (promise === resultOrReason) {
+function pRP(me, resultOrReason, resolve, reject) {
+  if (me === resultOrReason) {
     const reason = new TypeError('Can not fulfill promise with itself')
     return reject(reason)
   }
-  if (resultOrReason instanceof A) {
-    // If x is pending, promise must remain pending until x is fulfilled or rejected.
-    if (resultOrReason.state == PENDING) return resultOrReason.then(()=>{
-      resolve(resultOrReason.result)
-    },()=>{
-      reject(resultOrReason.result)
-    })
-    else if (resultOrReason.state == FULFILLED) return resolve(resultOrReason.result)
-    else return reject(resultOrReason.result)
-  }
+  // resultOrReason是promise的话，promise也是object，也有then函数，处理是一样的。目的就是复用resultOrReason的状态
   if (isObject(resultOrReason) || isFunction(resultOrReason)) {
     try {
       const then = resultOrReason.then
       if (isFunction(then)) {
         let promiseCalled = false
         const resolvePromise = (y) => {
-          // TODO
+          // 只回掉一次
           if (promiseCalled) return
           promiseCalled = true
-          pRP(promise, y, resolve, reject)
+          // 递归
+          pRP(me, y, resolve, reject)
         }
         const rejectPromise = (r) => {
-          // TODO
+          // 只回掉一次
           if (promiseCalled) return
           promiseCalled = true
           reject(r)
@@ -122,6 +103,7 @@ function pRP(promise, resultOrReason, resolve, reject) {
             // 要用return终止分支
             return reject(err)
           } else {
+            // ignore
             return
           }
         }
